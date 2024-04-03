1f6d81d12bcf52e07cc947ac5a0d218e35871f23
Ray Chung <rchung725@gmail.com>
2024-04-02 21:46:43 -0400

    first prototype

---
+++ ios-prototype/1/ios-proto/ios_protoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_protoApp.swift
+//  ios-proto
+//
+//  Created by Raymond Yongsub Chung on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_protoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
